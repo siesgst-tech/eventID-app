9b209108af7c048a8e9eb591e795e1f65a8fa81e
roh-7 <rohit.6233@outlook.com>
2017-07-15 20:05:21 +0530

    [design]: forgot password

--- app/src/main/java/com/example/event/Login.java
+++ app/src/main/java/com/example/event/Login.java
@@ -1,18 +1,23 @@
 package com.example.event;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity
 {
 	private static final String TAG = Login.class.getSimpleName();
 	EditText email, password;
 	Button login;
+	TextView forgot;
 	String email_entered, password_entered;
+	RelativeLayout login_layout, forgot_layout;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -22,6 +27,9 @@ public class Login extends AppCompatActivity
 		email = (EditText) findViewById(R.id.email);
 		password = (EditText) findViewById(R.id.password);
 		login = (Button) findViewById(R.id.login_btn);
+		forgot = (TextView) findViewById(R.id.forgot_textView);
+		login_layout = (RelativeLayout) findViewById(R.id.login_layout);
+		forgot_layout = (RelativeLayout) findViewById(R.id.forgot_layout);
 		login.setOnClickListener(new View.OnClickListener()
 		{
 			@Override
@@ -29,11 +37,29 @@ public class Login extends AppCompatActivity
 			{
 				email_entered = email.getText().toString();
 				password_entered = password.getText().toString();
+				if (email_entered.length() == 0)
+				{
+					email.setError("Please enter your email");
+				}
+				else if (password_entered.length() == 0)
+				{
+					Snackbar.make(findViewById(R.id.activity_login), "Please enter your password.", Snackbar.LENGTH_SHORT).show();
+				}
 				Log.v(TAG, "email: " + email_entered);
 				Log.v(TAG, "password: " + password_entered);
 				email.setText("");
 				password.setText("");
 			}
 		});
+		forgot.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				login_layout.setVisibility(View.GONE);
+				forgot_layout.setVisibility(View.VISIBLE);
+			}
+		});
 	}
+	
 }
--- app/src/main/java/com/example/event/Splash.java
+++ app/src/main/java/com/example/event/Splash.java
@@ -5,6 +5,7 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class Splash extends AppCompatActivity
@@ -20,6 +21,9 @@ public class Splash extends AppCompatActivity
 		TextView splash = (TextView) findViewById(R.id.splash_txt);
 		Typeface typeface = Typeface.createFromAsset(getAssets(),"fonts/OpenSans-Light.ttf");
 		splash.setTypeface(typeface);
+		View decorView = getWindow().getDecorView();
+		int visibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+		decorView.setSystemUiVisibility(visibility);
 		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run()
