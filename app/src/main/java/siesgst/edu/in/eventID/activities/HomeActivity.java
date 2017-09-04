73ed9ebcdb84f02be7cd529058e48be7bca35db9
DrBanner97 <arn884770@gmail.com>
2017-09-05 00:07:40 +0530

    test push

--- app/src/main/java/siesgst/edu/in/eventID/activities/HomeActivity.java
+++ app/src/main/java/siesgst/edu/in/eventID/activities/HomeActivity.java
@@ -56,6 +56,8 @@ public class HomeActivity extends AppCompatActivity
 		adapter.addFrag(messagesFragment, "Messages");
 		viewPager.setAdapter(adapter);
 	}
+
+	
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
